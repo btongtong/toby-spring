ded013e9fe965bcab215040ce922c5468c83fca4
btongtong <leeym2615@naver.com>
2024-09-26 22:45:10 +0900

    서블릿 컨터에너에 서블릿 등록하기

--- src/main/java/study/tobyspring/TobySpringApplication.java
+++ src/main/java/study/tobyspring/TobySpringApplication.java
@@ -1,14 +1,29 @@
 package study.tobyspring;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+
+import java.io.IOException;
 
 public class TobySpringApplication {
 
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {    // 서블릿 등록
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setStatus(200);
+                    resp.setHeader("Content-Type", "text/plain");
+                    resp.getWriter().println("Hello Servlet");
+                }
+            }).addMapping("/hello");
+        });
         webServer.start();
     }
 
