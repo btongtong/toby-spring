c5944c486132498ebfee7f7e5feefcf313e11528
btongtong <leeym2615@naver.com>
2024-09-27 23:37:07 +0900

    스프링 컨테이너 생성 및 빈 등록

--- src/main/java/study/tobyspring/TobySpringApplication.java
+++ src/main/java/study/tobyspring/TobySpringApplication.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -17,26 +18,26 @@ import java.io.IOException;
 public class TobySpringApplication {
 
     public static void main(String[] args) {
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
+
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {    // 서블릿 등록
             servletContext.addServlet("frontcontroller", new HttpServlet() {
-                HelloController helloController = new HelloController();
-
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
                     // 인증, 보안, 다국어, 공통 기능
                     if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) { // 매핑
                         String name = req.getParameter("name"); // 웹 요청 로직을 처리할 때 쓰일 평범한 자바 데이터 타입으로 바인딩
 
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
                         String ret = helloController.hello(name);
 
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                         resp.getWriter().println(ret);
                     }
-                    else if(req.getRequestURI().equals("/user")) {
-                        //
-                    } else {
+                    else {
                         resp.setStatus(HttpStatus.NOT_FOUND.value());
                     }
                 }
