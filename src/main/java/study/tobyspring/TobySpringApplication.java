c53a12a1fe64b8f1b34702a2dd08d92ebfa8ee6a
btongtong <leeym2615@naver.com>
2024-09-28 23:47:48 +0900

    디스패처서블릿으로 컨트롤러에 어노테이션 읽어서 자동으로 매핑하도록 변경

--- src/main/java/study/tobyspring/TobySpringApplication.java
+++ src/main/java/study/tobyspring/TobySpringApplication.java
@@ -11,37 +11,24 @@ import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import java.io.IOException;
 
 public class TobySpringApplication {
 
     public static void main(String[] args) {
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
 
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {    // 서블릿 등록
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // 인증, 보안, 다국어, 공통 기능
-                    if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) { // 매핑
-                        String name = req.getParameter("name"); // 웹 요청 로직을 처리할 때 쓰일 평범한 자바 데이터 타입으로 바인딩
-
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String ret = helloController.hello(name);
-
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(ret);
-                    }
-                    else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-                }
-            }).addMapping("/*");
+            servletContext.addServlet("dispatcherServlet",
+                        new DispatcherServlet(applicationContext)
+                    ).addMapping("/*");
         });
         webServer.start();
     }
