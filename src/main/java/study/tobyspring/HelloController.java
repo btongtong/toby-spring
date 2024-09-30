2a02b4cc8f20488c643bbacf162c36e68038e6d6
btongtong <leeym2615@naver.com>
2024-09-30 21:42:43 +0900

    ApplicationContext가 set으로 주입 된다 = 스프링 컨테이너에 ApplicationContext 구성정보가 있다 = 생성자를 통해 주입 가능하다

--- src/main/java/study/tobyspring/HelloController.java
+++ src/main/java/study/tobyspring/HelloController.java
@@ -1,20 +1,21 @@
 package study.tobyspring;
 
-import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RestController
-public class HelloController implements ApplicationContextAware {
+public class HelloController {
     private final HelloService helloService;
-    private ApplicationContext applicationContext;  // 생성자가 다 만들어진 이후에 set 메서드를 통해 초기화 되기 때문에 final 키워드 붙이면 안됨
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
+
+        System.out.println(applicationContext);
     }
 
     @GetMapping("/hello")
@@ -22,9 +23,4 @@ public class HelloController implements ApplicationContextAware {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-        System.out.println(applicationContext);
-    }
 }
