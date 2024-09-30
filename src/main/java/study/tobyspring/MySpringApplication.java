6e3f4a9ca86ca12386a09aea2e0eec1549fd05c4
btongtong <leeym2615@naver.com>
2024-09-30 21:53:30 +0900

    스프링/서블릿 컨테이너, 컨텍스트 만드는 로직 run 메서드로 분리

---
+++ src/main/java/study/tobyspring/MySpringApplication.java
@@ -0,0 +1,28 @@
+package study.tobyspring;
+
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MySpringApplication {
+    public static void run(Class<?> applicationClass, String... args) {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {    // 서블릿 등록
+                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+                            .addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
+        applicationContext.register(applicationClass);   // 컨테이너를 구성하는데 필요한 설정이 들은 자바 클래스 정보를 등록
+        applicationContext.refresh();
+    }
+}
