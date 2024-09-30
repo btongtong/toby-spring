bfbd90ce69441d89f1cf34ef4407fd75a0501db4
btongtong <leeym2615@naver.com>
2024-09-30 21:39:05 +0900

    서블릿 컨테이너, 프론트 컨트롤러(디스패처 서블릿)도 빈으로 등록(팩토리 메서드 방식), 스프링 컨테이너가 ApplicationContextAware의 set 메서드를 통해 디스패처 서블릿에 스프링 컨텍스트 자동 주입

--- src/main/java/study/tobyspring/HelloController.java
+++ src/main/java/study/tobyspring/HelloController.java
@@ -1,13 +1,17 @@
 package study.tobyspring;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RestController
-public class HelloController {
+public class HelloController implements ApplicationContextAware {
     private final HelloService helloService;
+    private ApplicationContext applicationContext;  // 생성자가 다 만들어진 이후에 set 메서드를 통해 초기화 되기 때문에 final 키워드 붙이면 안됨
 
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
@@ -16,5 +20,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+        System.out.println(applicationContext);
     }
 }
--- src/main/java/study/tobyspring/TobySpringApplication.java
+++ src/main/java/study/tobyspring/TobySpringApplication.java
@@ -3,6 +3,7 @@ package study.tobyspring;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -11,6 +12,15 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class TobySpringApplication {
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
 
     public static void main(String[] args) {
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
@@ -18,11 +28,12 @@ public class TobySpringApplication {
             protected void onRefresh() {
                 super.onRefresh();
 
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+
                 WebServer webServer = serverFactory.getWebServer(servletContext -> {    // 서블릿 등록
-                    servletContext.addServlet("dispatcherServlet",
-                            new DispatcherServlet(this)
-                    ).addMapping("/*");
+                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+                            .addMapping("/*");
                 });
                 webServer.start();
             }
