0075e8d6c3c413699fcaab9ff89249e873f033ef
btongtong <leeym2615@naver.com>
2024-10-03 13:39:51 +0900

    Composed Annotation으로 여러 에노테이션 효과를 한번에 적용, 빈 만드는 config클래스 새로 생성

--- src/test/java/study/tobyspring/HelloServiceTest.java
+++ src/test/java/study/tobyspring/HelloServiceTest.java
@@ -2,29 +2,10 @@ package study.tobyspring;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 import static org.assertj.core.api.Assertions.*;
 
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-@UnitTest
-@interface FastUnitTest {
-
-}
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
-@Test
-@interface UnitTest {
-}
-
 public class HelloServiceTest {
-    @FastUnitTest
+    @Test
     public void simpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
 
