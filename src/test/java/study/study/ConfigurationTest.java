791ac8646e4b22d04786582ddfc907b6a59ecbf3
btongtong <leeym2615@naver.com>
2024-10-04 17:01:53 +0900

    스프링 컨테이너 구성정보로 사용되기 전 Configuration 테스트

---
+++ src/test/java/study/study/ConfigurationTest.java
@@ -0,0 +1,62 @@
+package study.study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+public class ConfigurationTest {
+    @Test
+    void configuration() {
+        // 스프링 컨테이너의 구성정보로 사용되기 전은 bean1.common과 bean2.common이 같지 않다: Common을 매번 생성한다
+        MyConfig myConfig = new MyConfig();
+        Bean1 bean1 = myConfig.bean1();
+        Bean2 bean2 = myConfig.bean2();
+
+        Assertions.assertThat(bean1.common).isSameAs(bean2.common);
+    }
+
+    @Configuration
+    static class MyConfig {
+        @Bean
+        Common common() {
+            return new Common();
+        }
+
+        @Bean
+        Bean1 bean1() {
+            return new Bean1(common());
+        }
+
+        @Bean
+        Bean2 bean2() {
+            return new Bean2(common());
+        }
+    }
+
+    static class Bean1 {
+        private final Common common;
+
+        Bean1(Common common) {
+            this.common = common;
+        }
+    }
+
+    static class Bean2 {
+        private final Common common;
+
+        Bean2(Common common) {
+            this.common = common;
+        }
+    }
+
+    static class Common {
+
+    }
+
+    // Bean1 <-- Common
+    // Bean2 <-- Common
+
+}
