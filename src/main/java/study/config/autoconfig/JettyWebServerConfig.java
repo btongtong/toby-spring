3b569268ba52f852493229ebe08e72774a160067
btongtong <leeym2615@naver.com>
2024-10-05 14:48:55 +0900

    gradle에 Jetty 스타터 추가, JettyWebServerFactory서버 구성 추가

--- src/main/java/study/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/study/config/autoconfig/JettyWebServerConfig.java
@@ -1,14 +1,14 @@
 package study.config.autoconfig;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import study.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
-    @Bean("TomcatWebServerFactory")
+public class JettyWebServerConfig {
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 }
