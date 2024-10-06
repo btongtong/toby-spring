ffc713b52972402eed2a9715c52228b99f849fab
btongtong <leeym2615@naver.com>
2024-10-06 22:31:34 +0900

    톰켓서버 컨텍스트패스 설정 Environment 주입받아서 하기'

--- src/main/java/study/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/study/config/autoconfig/TomcatWebServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import study.config.ConditionalMyOnClass;
 import study.config.MyAutoConfiguration;
 
@@ -12,7 +13,9 @@ import study.config.MyAutoConfiguration;
 public class TomcatWebServerConfig {
     @Bean("TomcatWebServerFactory")
     @ConditionalOnMissingBean   // 해당 빈과 같은 타입의 빈이 이미 개발자가 구성정보로 만들었는지 체크 후 빈 등록
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setContextPath(env.getProperty("contextPath"));
+        return factory;
     }
 }
