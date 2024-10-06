52c91c866546f5cba2d16d6924c083c95da780be
btongtong <leeym2615@naver.com>
2024-10-06 22:40:42 +0900

    치환자 붙은 필드를 프로퍼티로 교체해주는 후처리 기능을 하는 클래스 스프링 컨테이너에 등록. 필드를 통해 톰캣 서버 컨텍스트패스 설정

--- src/main/java/study/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/study/config/autoconfig/TomcatWebServerConfig.java
@@ -1,21 +1,24 @@
 package study.config.autoconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import study.config.ConditionalMyOnClass;
 import study.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("TomcatWebServerFactory")
     @ConditionalOnMissingBean   // 해당 빈과 같은 타입의 빈이 이미 개발자가 구성정보로 만들었는지 체크 후 빈 등록
-    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+    public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 }
