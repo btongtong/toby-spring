5846df7603d9c9645534084c9f9754a4bb25e11a
btongtong <leeym2615@naver.com>
2024-10-06 22:25:08 +0900

    스프링 컨테이너 준비 완료 후 실행되는 ApplicationRunner 구현해서 Envirionment 주입 받고 프로퍼티 확인하기

--- src/main/java/study/tobyspring/TobySpringApplication.java
+++ src/main/java/study/tobyspring/TobySpringApplication.java
@@ -1,10 +1,22 @@
 package study.tobyspring;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import study.config.MySpringBootApplication;
 
 @MySpringBootApplication
 public class TobySpringApplication {
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment env) {
+        return args -> {
+            String name = env.getProperty("my.name");
+            System.out.println("my.name: " + name);
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(TobySpringApplication.class, args);
     }
