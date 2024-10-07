06b2f09f5bd15aac1c5a49004a089842eb5f312d
btongtong <leeym2615@naver.com>
2024-10-07 14:34:36 +0900

    바인더로 세터 존재하는 필드 이름과 같은 envrionment property 바인딩하도록 변경

--- src/main/java/study/config/autoconfig/ServerPropertiesConfig.java
+++ src/main/java/study/config/autoconfig/ServerPropertiesConfig.java
@@ -1,5 +1,6 @@
 package study.config.autoconfig;
 
+import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import study.config.MyAutoConfiguration;
@@ -8,11 +9,6 @@ import study.config.MyAutoConfiguration;
 public class ServerPropertiesConfig {
     @Bean
     public ServerProperties serverProperties(Environment environment) {
-        ServerProperties properties = new ServerProperties();
-
-        properties.setContextPath(environment.getProperty("contextPath"));
-        properties.setPort(Integer.parseInt(environment.getProperty("port")));
-
-        return properties;
+        return Binder.get(environment).bind("", ServerProperties.class).get();
     }
 }
