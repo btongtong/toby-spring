310219f71a0ff6c10cccf9e9fba7cdd3c82e3fbb
btongtong <leeym2615@naver.com>
2024-10-10 13:59:10 +0900

    JdbcTemplate, JdbcTransactionManager 구성하고 데이터베이스에 insert, transaction 테스트

--- src/test/java/study/tobyspring/DataSourceTest.java
+++ src/test/java/study/tobyspring/DataSourceTest.java
@@ -1,19 +1,13 @@
 package study.tobyspring;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = TobySpringApplication.class)
-@TestPropertySource("classpath:/application.properties")    // application.properties를 등록하는 것은 springboot의 기본 동작 방식이 아니고, 후처리를 통한 것이니까 따로 읽어오기
+@HellobootTest
     public class DataSourceTest {
         @Autowired
         DataSource dataSource;
